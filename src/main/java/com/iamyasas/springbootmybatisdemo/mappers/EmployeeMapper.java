8629d474efc352cdc630ff6f1d4751675ecd8a71
Yasas Karunarathna <yasasmail@gmail.com>
2018-09-26 00:08:29 +0530

    Added @Param to the mapper interface methods' parameters

--- src/main/java/com/iamyasas/springbootmybatisdemo/mappers/EmployeeMapper.java
+++ src/main/java/com/iamyasas/springbootmybatisdemo/mappers/EmployeeMapper.java
@@ -1,14 +1,15 @@
 package com.iamyasas.springbootmybatisdemo.mappers;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import com.iamyasas.springbootmybatisdemo.models.Employee;
 
 @Mapper
 public interface EmployeeMapper {
 	
-	public Employee[] getEmployees(String name);
+	public Employee[] getEmployees(@Param("name") String name);
 	
-
-	public Employee getEmployee(int employeeID);
+	public Employee getEmployee(@Param("employeeID") int employeeID);
 	
 }
